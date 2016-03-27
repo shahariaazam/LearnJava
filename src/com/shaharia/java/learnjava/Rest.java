f9e6bc79343e186a0d158fb097f40326c11f5996
Shaharia Azam <shaharia.azam@gmail.com>
2016-03-27 19:39:17 +0600

    sample json url replaced

--- src/com/shaharia/java/learnjava/Rest.java
+++ src/com/shaharia/java/learnjava/Rest.java
@@ -5,7 +5,7 @@ import br.com.caelum.restfulie.Restfulie;
 
 public class Rest {
     public static void main(String[] args){
-        Response r = Restfulie.at("http://api.miteksoftware.com/v1/whmcs/client").get();
+        Response r = Restfulie.at("http://ip.jsontest.com/").get();
         System.out.println(r.getContent().toCharArray());
     }
 }
